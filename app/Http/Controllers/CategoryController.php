fe00b7c50345d9a2d0f116287e52cb57bf7bc926
Seri-otsuka <kd1326982@st.kobedenshi.ac.jp>
2023-12-01 14:55:34 +0900

    CategoryConttorolleの編集

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     //
+     //表示一覧のメソッド
+    public function category(Category $category, User $user)
+    {
+        
+        return view('categories.index')->with([
+            'users' => $user,
+            'categories' => $category->getPaginateByLimit(20)]);
+    }
 }
