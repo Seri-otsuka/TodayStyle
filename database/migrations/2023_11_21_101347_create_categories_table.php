6e9e2642ca412fc198d12428d1ad20c1fe9c88a1
Seri-otsuka <kd1326982@st.kobedenshi.ac.jp>
2023-12-15 14:27:18 +0900

    データベース各所/seeder実行

--- database/migrations/2023_11_21_101347_create_categories_table.php
+++ database/migrations/2023_11_21_101347_create_categories_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name',100);
-            $table->string('image');
+            $table->string('image_true');
+            $table->string('image_false');
             $table->timestamp('created_at')->useCurrent();
             $table->timestamp('updated_at')->useCurrent();
             $table->softDeletes();
